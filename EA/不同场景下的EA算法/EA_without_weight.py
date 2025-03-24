b093032fae9723c3031772beaa4cfd0c10d7a574
uDeserve <1157548404@qq.com>
2025-03-24 15:35:55 +0800

    Create EA_without_weight.py

---
+++ EA/不同场景下的EA算法/EA_without_weight.py
@@ -0,0 +1,22 @@
+def EA(x):
+    """
+    Parameters
+    ----------
+    x : numpy array
+        data of shape (num_samples, num_channels, num_time_samples)
+
+    Returns
+    ----------
+    XEA : numpy array
+        data of shape (num_samples, num_channels, num_time_samples)
+    """
+    cov = np.zeros((x.shape[0], x.shape[1], x.shape[1]))
+    for i in range(x.shape[0]):
+        cov[i] = np.cov(x[i])
+    refEA = np.mean(cov, 0)
+    refEA += 1e-6 * np.eye(refEA.shape[0])  # 添加微小单位矩阵，作为正则化项
+    sqrtRefEA = fractional_matrix_power(refEA, -0.5)
+    XEA = np.zeros(x.shape)
+    for i in range(x.shape[0]):
+        XEA[i] = np.dot(sqrtRefEA, x[i])
+    return XEA
